c83c34165134ef6939a6ca6ac1bb90e4f4d5fe6d
Mehul Parmar <mehulasjack@gmail.com>
2023-11-02 17:58:36 +0530

    Updated

--- Source/include/MDCAlertController+MaterialTheming.h
+++ Source/include/MDCAlertController+MaterialTheming.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "MaterialDialogs.h>
+#import "MaterialDialogs.h"
 #import "MaterialContainerScheme.h"
 
 /**
